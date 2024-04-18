f8ab27e6f0530486c504818641bf668735030507
Jason Lin <shashhhh123@gmail.com>
2024-04-18 15:26:39 -0400

    Finished talent scraper
    Won't let me commit from vs code

---
+++ CFB_talent_scraper.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import cfbd
+from cfbd.rest import ApiException
+from bs4 import BeautifulSoup as BS
+import sqlite3
+configuration = cfbd.Configuration()
+configuration.api_key['Authorization'] = api_key
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+api_instance = cfbd.TeamsApi(cfbd.ApiClient(configuration))
+year = 2023
+
+def read_api_key(file_path="api_key.txt"):
+    with open(file_path, "r") as file:
+        api_key = file.read().strip()
+        return api_key
+    
+api_key = read_api_key()
+
+def create_database(table):
+    conn = sqlite3.connect('CFB_teams.db')
+    c = conn.cursor()
+    c.execute(f'''CREATE TABLE IF NOT EXISTS {table} (
+                id INTEGER PRIMARY KEY,
+                School TEXT,
+                Talent FLOAT
+                )''')
+    conn.commit()
+    conn.close()
+
+def insert_data(data, table):
+    conn = sqlite3.connect('CFB_teams.db')
+    c = conn.cursor()
+    c.executemany(f"INSERT INTO {table} (School, Talent) VALUES (?, ?)", data)
+    conn.commit()
+    conn.close()
+
+def get_last_id(table):
+    conn = sqlite3.connect('CFB_teams.db')
+    c = conn.cursor()
+    c.execute(f"SELECT MAX(id) FROM {table}")
+    last_id = c.fetchone()[0]
+    conn.close()
+    return last_id if last_id else 0 
+
+
+def inserter(data, table):
+    last_id = get_last_id(table)
+    if (last_id < 200):
+        new_data = data[last_id:last_id+25]
+        insert_data(new_data, table)
+
+
+try:
+    api_response = api_instance.get_talent(year=year)
+except ApiException as e:
+    print("Exception when calling TeamsApi->get_talent: %s\n" % e)
+data = []
+
+for team in api_response:
+    data.append((team.school, team.talent))
+
+create_database('team_talent')
+inserter(data, 'team_talent')
---
+++ CFB_team_wins_scraper.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import cfbd
+from cfbd.rest import ApiException
+import CFB_talent_scraper as scraper
+from bs4 import BeautifulSoup as BS
+import sqlite3
+configuration = cfbd.Configuration()
+configuration.api_key['Authorization'] = scraper.api_key
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
+year = 2023
+
+def create_database(table):
+     conn = sqlite3.connect('CFB_teams.db')
+     c = conn.cursor()
+     c.execute(f'''CREATE TABLE IF NOT EXISTS {table} (
+                id INTEGER PRIMARY KEY,
+                Wins INT
+                )''')
+     conn.commit()
+     conn.close()
+
+
+
+
+def get_team_wins(table):
+    conn = sqlite3.connect('CFB_teams.db')
+    c = conn.cursor()
+    last_id = scraper.get_last_id('team_talent')
+    for i in range(last_id, last_id + 25):
+        c.execute(f"SELECT * FROM {table} WHERE id=?", (i,))
+        team = c.fetchone()
+        try:
+            api_response = api_instance.get_team_records(year=year)
+        except ApiException as e:
+            print("Exception when calling GamesApi->get_team_records: %s\n" % e)
+
+data = []
+
+create_database('team_wins')
