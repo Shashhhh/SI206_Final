36f09294b499066834cbf475e5ae766e177db408
Jason Lin <linjay@umich.edu>
2024-04-19 21:01:34 -0400

    different color bars for graph

--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,42 @@
 import sqlite3
 import matplotlib.pyplot as plt
 
-conn = sqlite3.connect('main_db.db')
+team_colors = {
+    'Arizona Cardinals': '#97233F',    # Cardinal Red
+    'Atlanta Falcons': '#A71930',       # Falcon Red
+    'Baltimore Ravens': '#241773',      # Raven Purple
+    'Buffalo Bills': '#00338D',         # Bills Blue
+    'Carolina Panthers': '#0085CA',     # Panther Blue
+    'Chicago Bears': '#0B162A',         # Bear Navy Blue
+    'Cincinnati Bengals': '#FB4F14',    # Bengal Orange
+    'Cleveland Browns': '#311D00',      # Brown Orange
+    'Dallas Cowboys': '#041E42',        # Cowboys Navy Blue
+    'Denver Broncos': '#002244',        # Bronco Navy Blue
+    'Detroit Lions': '#0076B6',         # Lion Blue
+    'Green Bay Packers': '#203731',     # Packer Green
+    'Houston Texans': '#03202F',        # Texan Navy Blue
+    'Indianapolis Colts': '#002C5F',    # Colt Blue
+    'Jacksonville Jaguars': '#006778',  # Jaguar Teal
+    'Kansas City Chiefs': '#E31837',    # Chief Red
+    'Las Vegas Raiders': '#000000',      # Raider Black
+    'Los Angeles Chargers': '#002A5E',  # Charger Navy Blue
+    'Los Angeles Rams': '#002244',      # Ram Navy Blue
+    'Miami Dolphins': '#008E97',        # Dolphin Aqua
+    'Minnesota Vikings': '#4F2683',     # Viking Purple
+    'New England Patriots': '#002244',  # Patriot Blue
+    'New Orleans Saints': '#D3BC8D',    # Saint Gold
+    'New York Giants': '#0B2265',       # Giant Blue
+    'New York Jets': '#125740',         # Jet Green
+    'Philadelphia Eagles': '#004C54',   # Eagle Green
+    'Pittsburgh Steelers': '#FFB612',   # Steeler Gold
+    'San Francisco 49ers': '#AA0000',   # 49er Scarlet Red
+    'Seattle Seahawks': '#69BE28',      # Seahawk Green
+    'Tampa Bay Buccaneers': '#D50A0A',  # Buccaneer Red
+    'Tennessee Titans': '#0C2340',      # Titan Navy Blue
+    'Washington Football Team': '#773141'  # Washington Burgundy
+}
+
+conn = sqlite3.connect('Main_db.db')
 c = conn.cursor()
 
 c.execute('''
@@ -20,11 +55,19 @@ conn.close()
 teams = [row[0] for row in results]
 num_players = [row[1] for row in results]
 
-plt.figure(figsize=(10, 6))
-plt.bar(teams, num_players, color='skyblue')
+plt.figure(figsize=(14, 8))
+bars = plt.bar(teams, num_players)
+
+for bar, team in zip(bars, teams):
+    bar.set_color(team_colors.get(team, 'gray'))  
+
 plt.xlabel('Team')
 plt.ylabel('Number of Players with Fantasy Points > 200')
 plt.title('Number of Players on Each Team with Fantasy Points > 200')
-plt.xticks(rotation=45, ha='right')
+
+for bar, num in zip(bars, num_players):
+    plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 0.05, num, ha='center', va='bottom')
+
+plt.xticks(rotation=90)
 plt.tight_layout()
 plt.show()
