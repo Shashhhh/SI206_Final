63acb725cf8b29a966c8a8d9af085922eea817a4
Jason Lin <linjay@umich.edu>
2024-04-19 21:54:22 -0400

    combined tables for CFB_getter

--- CFB_getter.py
+++ CFB_getter.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import cfbd
 from cfbd.rest import ApiException
-from bs4 import BeautifulSoup as BS
 import sqlite3
 
 def read_api_key(file_path="api_key.txt"):
@@ -10,36 +9,26 @@ def read_api_key(file_path="api_key.txt"):
         return api_key
 
 def create_database():
-    conn = sqlite3.connect('CFB_teams.db')
+    conn = sqlite3.connect('Main_db.db')
     c = conn.cursor()
-    c.execute(f'''CREATE TABLE IF NOT EXISTS team_talent (
-                team_id INTEGER PRIMARY KEY,
-                School TEXT,
-                Talent FLOAT
-                )''')
-    c.execute(f'''CREATE TABLE IF NOT EXISTS team_wins (
+    c.execute(f'''CREATE TABLE IF NOT EXISTS CFB_teams (
                 id INTEGER PRIMARY KEY,
+                School TEXT,
+                Talent FLOAT,
                 Wins INTEGER
                 )''')
     conn.commit()
     conn.close()
 
 def insert_data(data, table):
-    conn = sqlite3.connect('CFB_teams.db')
+    conn = sqlite3.connect('Main_db.db')
     c = conn.cursor()
-    c.executemany(f"INSERT INTO {table} (School, Talent) VALUES (?, ?)", data)
-    conn.commit()
-    conn.close()
-
-def insert_wins(data, table):
-    conn = sqlite3.connect('CFB_teams.db')
-    c = conn.cursor()
-    c.executemany(f"INSERT INTO {table} (Wins) VALUES (?)", [(wins,) for wins in data])
+    c.executemany(f"INSERT INTO {table} (School, Talent, Wins) VALUES (?, ?, ?)", data)
     conn.commit()
     conn.close()
 
 def get_last_id(table):
-    conn = sqlite3.connect('CFB_teams.db')
+    conn = sqlite3.connect('Main_db.db')
     c = conn.cursor()
     c.execute(f"SELECT MAX(id) FROM {table}")
     last_id = c.fetchone()[0]
@@ -49,13 +38,7 @@ def get_last_id(table):
 def inserter(data, table):
     last_id = get_last_id(table)
     if (last_id < 200):
-        new_data = data[last_id:last_id+10]
-        win_data = []
-        for tup in new_data:
-            wins_response = wins_api.get_team_records(year=year, team = tup[0])
-            win_data.append(wins_response[0].total.wins)
-        insert_data(new_data, table)
-        insert_wins(win_data, 'team_wins')
+        insert_data(data, table)
 
 
 api_key = read_api_key()
@@ -70,11 +53,14 @@ try:
     api_response = talent_api.get_talent(year=year)
 except ApiException as e:
     print("Exception when calling TeamsApi->get_talent: %s\n" % e)
-    
-data = []
-
-for team in api_response:
-    data.append((team.school, team.talent))
 
 create_database()
-inserter(data, 'team_talent')
+data = []
+last_id = get_last_id('CFB_teams')
+api_response = api_response[last_id:last_id+25]
+for team in api_response:
+    wins_response = wins_api.get_team_records(year=year, team = team.school)
+    data.append((team.school, team.talent, wins_response[0].total.wins))
+
+
+inserter(data, 'CFB_teams')
